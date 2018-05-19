965e37d1876e579329e91277612b99c26cf6e1cc
mberaha <mario@ndg.ai>
2018-05-19 19:30:45 +0200

    reverting some files to master

--- src/encoder.py
+++
@@ -1,6 +0,0 @@
-from src.rnn import Rnn
-
-
-class Encoder(Rnn):
-    def __init__(self, input_size, hidden_size, num_layers):
-        super().__init__(input_size, hidden_size, num_layers)
--- src/generator.py
+++
@@ -1,7 +0,0 @@
-from src.rnn import Rnn
-
-
-class Generator(Rnn):
-
-    def __init__(self, input_size, hidden_size, num_layers):
-        super().__init__(input_size, hidden_size, num_layers)
