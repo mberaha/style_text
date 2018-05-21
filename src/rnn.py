07f694acb799e94cf6f4a9eda98fcb0a9aa61532
WolfLo <lorenzo.lupo93@gmail.com>
2018-05-21 21:13:47 +0200

    troubles with tensor dims

--- src/rnn.py
+++ src/rnn.py
@@ -3,24 +3,20 @@ from torch import nn
 
 
 class Rnn(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers):
+    def __init__(self, input_size, hidden_size, num_layers, device):
         # call the __init__ of nn.Module and inherit its functions
         super().__init__()
+        # device storing hidden states
+        self.device = device
         # size of embeddings
         self.input_size = input_size
         # size of the GRU ouput = y + z
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         # create the RNN cell
-        self._createGRU()
-
-    def _createGRU(self):
         self.cell = nn.GRU(
             self.input_size, self.hidden_size, self.num_layers)
 
     def forward(self, input, hidden):
         output, hidden = self.cell(input, hidden)
         return output, hidden
-
-    def initHidden(self, device):
-        return torch.zeros(1, 1, self.hidden_size, device=device)
