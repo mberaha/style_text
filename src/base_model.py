b342338d7a6a6c243f50aeb040b06da709d80b61
mariob6 <berahamario@gmail.com>
2018-05-25 19:32:52 +0200

    base model +training

--- src/base_model.py
+++ src/base_model.py
@@ -1,3 +1,4 @@
+import random
 import torch
 from torch import nn
 from tqdm import tqdm
@@ -13,12 +14,22 @@ class BaseModel(nn.Module):
     def load(self):
         self.load_state_dict(self.savefile)
 
-    def train(self, trainBatches, validBatch):
+    def train(self, trainBatches, validBatch, shuffle=True):
+        for epoch in range(self.params.epochs):
+            if shuffle:
+                random.shuffle(trainBatches)
+            self.runEpoch(trainBatches, validBatch)
+
+    def runEpoch(self, trainBatches, validBatch):
         bestLoss = self._MAX_LOSS
-        for index in tqdm(range(len(trainBatches))):
+        progbar = tqdm(range(len(trainBatches)))
+        for index in progbar:
             inputs, labels = trainBatches[index]
-            self.trainOnBatch(inputs, labels)
+            loss = self.trainOnBatch(inputs, labels)
+            progbar.set_description("Loss: {0}".format(loss))
 
         evaluationLoss = self.evaluate(*validBatch)
+        print("Epoch {0}/{1}, Loss on evaluation set: {2}".format(
+            index, len(trainBatches), evaluationLoss))
         if evaluationLoss < bestLoss:
             torch.save(self.state_dict())
