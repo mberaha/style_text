a97651e5e93f8cf1777ee8c120b36216d7e2dacd
mariob6 <berahamario@gmail.com>
2018-06-15 13:17:01 +0200

    not very promising results

--- src/beam_search.py
+++ src/beam_search.py
@@ -21,12 +21,11 @@ class BeamState(object):
 
 class Decoder(object):
 
-    def __init__(self, rnn, hToVocab, vocabulary, max_length, beam_width):
-        self.vocabulary = vocabulary
+    def __init__(self, styleTransfer, max_length, beam_width, params):
+        self.model = styleTransfer
         self.max_length = max_length
         self.width = beam_width
-        self.generator = rnn
-        self.hToVocab = hToVocab
+        self.params = params
 
     def _decode(self, tokens, h):
         """
@@ -41,13 +40,12 @@ class Decoder(object):
         # embedding nn lookup
         # currTokens = torch.matmul(
         #     vocabProbs, self.vocabulary.embeddings.weight)
-
         currTokens = tokens
         currh = h
         # generate next h state and logit
         # generator needs input (seq_len, batch_size, input_size)
-        outs, h = self.generator(currTokens, currh, pad=False)
-        vocabLogits = self.hToVocab(outs[:, 0, :])
+        outs, h = self.model.generator(currTokens, currh, pad=False)
+        vocabLogits = self.model.hiddenToVocab(outs[:, 0, :])
         # smooth logits into the probabilities of each word
         vocabProbs = F.softmax(
             vocabLogits / self.params.temperature, dim=1)
@@ -65,45 +63,53 @@ class Decoder(object):
         Args:
             h0 -- the first hidden state of dim = dim_y + dim_z
         """
-
-        go = self.vocabulary.getEmbedding(['<go>']).squeeze(0)
-        batch_size = len(h0)
-        init_state = BeamState([go]*batch_size, h0,
-                               [[] for i in range(batch_size)], [0]*batch_size)
-        beam = torch.FloatTensor([init_state])
-        beam.to(device)
-        for _ in range(self.max_len):
-            storeBeamLayer = torch.FloatTensor(
-                [[] for _ in range(batch_size)])
-            storeBeamLayer.to(device)
+        batch_size = h0.shape[1]
+        go = torch.stack(list(map(
+            self.model.vocabulary, ['go'] * batch_size)))
+        # go = self.model.vocabulary.getEmbedding(['<go>'] * batch_size)
+        init_state = BeamState(
+            go,
+            h0,
+            [[] for i in range(batch_size)],
+            [0]*batch_size)
+        beam = [init_state]
+        for _ in range(self.max_length):
+            storeBeamLayer = [[] for _ in range(batch_size)]
             for state in beam:
                 logProbs, indices, h = self._decode(state.word, state.h)
                 for b in range(batch_size):
-                    for w in range(self.beam_width):
+                    for w in range(self.width):
                         storeBeamLayer[b].append(
-                            BeamState(indices[b, w], h[b],
+                            BeamState(indices[b, w], h[:, b, ],
                                       state.sentence[b] + [indices[b, w]],
                                       state.nll[b] - logProbs[b, w]))
 
-            beam = torch.FloatTensor(
-                [deepcopy(init_state) for _ in range(self.beam_width)])
-            beam.to(device)
+            beam = [init_state for _ in range(self.width)]
             for b in range(batch_size):
                 # sort beam states by their probability (cumulated nll)
                 # TODO check if performance increase by dividing nll
                 # by number of words
                 sortedBeamLayer = sorted(storeBeamLayer[b], key=lambda k: k.nll)
-                for w in range(self.beam_width):
+                for w in range(self.width):
                     beam[w].word[b] = sortedBeamLayer[w].word
-                    beam[w].h[b] = sortedBeamLayer[w].h
+                    beam[w].h[:, b, :] = sortedBeamLayer[w].h
                     beam[w].sentence[b] = sortedBeamLayer[w].sentence
                     beam[w].nll[b] = sortedBeamLayer[w].nll
 
         # Returning the ids of the beam_width most probable sentences' words.
-        sentences = beam[0].sent
-        sentences = \
-            [[self.vocabulary.id2word[i] for i in sent] for sent in sentences]
+        sentences = beam[0].sentence
+        sentences = [
+            [self.model.vocabulary.id2word[i] for i in sent]
+            for sent in sentences]
         # TODO strip the EOS
         # sentences = strip_eos(sentences)
 
         return sentences
+
+    def rewriteBatch(self, sentences, labels):
+        self.model.evaluateOnBatch(sentences, labels)
+        originalHiddens = self.model.originalHiddens
+        transformedHiddens = self.model.transformedHiddens
+        original = self._beamDecode(originalHiddens)
+        transformed = self._beamDecode(transformedHiddens)
+        return original, transformed
