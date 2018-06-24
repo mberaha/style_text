ff55cda2987e69d24ace690cfc40c8bfe0ef97ea
mariob6 <berahamario@gmail.com>
2018-06-24 15:31:05 +0200

    using generateWithPreviousOutput

--- src/greedy_decoding.py
+++ src/greedy_decoding.py
@@ -12,13 +12,22 @@ class Decoder(object):
         self.width = beam_width
         self.params = params
 
+    def _decodeModel(self, h0s):
+        batch_size = h0s.shape[1]
+        hiddens, tokens = self.model._generateWithPrevOutput(
+            h0s, self.max_len, evaluation=True, soft=False)
+        sentences = []
+        for sentIndex in range(batch_size):
+            sent = tokens[sentIndex, :]
+            sentences.append(
+                [self.model.vocabulary.id2word[int(sent[i])]
+                 for i in range(self.max_len)])
+        return sentences
+
     def _decode(self, h0s):
 
         batch_size = h0s.shape[1]
         hidden = h0s
-        hiddens = torch.zeros(batch_size, self.max_len,
-                              self.params.autoencoder.hidden_size,
-                              device=device)
         tokens = torch.zeros(batch_size, self.max_len, device=device)
         goEmbedding = self.model.vocabulary(['<go>']).squeeze(0)
         goEmbedding = goEmbedding.repeat(batch_size, 1)
@@ -28,12 +37,13 @@ class Decoder(object):
         for index in range(self.max_len):
             # generator need input (seq_len, batch_size, input_size)
             out, hidden = self.model.generator(currTokens, hidden, pad=False)
-            vocabLogits = self.model.hiddenToVocab(out[:, 0, :])
-            hiddens[:, index, :] = hidden
+            print('out.shape ', out.shape)
+            # vocabLogits = self.model.hiddenToVocab(out[:, 0, :])
+            vocabLogits = self.model.hiddenToVocab(out)
+            # hiddens[:, index, :] = hidden
+            print("vocabLogits.shape: ", vocabLogits.shape)
 
-            vocabProbs = nn.functional.softmax(
-                vocabLogits / self.params.temperature, dim=1)
-            _, argmax = vocabProbs.max(1)
+            _, argmax = vocabLogits[:, 0, :].max(1)
             tokens[:, index] = argmax
             currTokens = self.model.vocabulary(argmax)
             currTokens = currTokens.unsqueeze(1)
@@ -50,7 +60,7 @@ class Decoder(object):
         self.model.transformBatch(sentences, labels)
         originalHiddens = self.model.originalHiddens
         transformedHiddens = self.model.transformedHiddens
-        original = self._decode(originalHiddens)
-        transformed = self._decode(transformedHiddens)
+        original = self._decodeModel(originalHiddens)
+        transformed = self._decodeModel(transformedHiddens)
 
         return original, transformed
