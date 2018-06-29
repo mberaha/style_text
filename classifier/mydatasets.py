eb0ba18aa622d7e065efde7313bea63c1fb2d819
mariob6 <berahamario@gmail.com>
2018-06-29 20:17:50 +0200

    classifier ready to train on yelp

--- classifier/mydatasets.py
+++ classifier/mydatasets.py
@@ -108,3 +108,82 @@ class MR(TarDataset):
 
         return (cls(text_field, label_field, examples=examples[:dev_index]),
                 cls(text_field, label_field, examples=examples[dev_index:]))
+class YELP(TarDataset):
+
+    # url = 'https://www.cs.cornell.edu/people/pabo/movie-review-data/rt-polaritydata.tar.gz'
+    # filename = 'rt-polaritydata.tar'
+    dirname = 'data/yelp/classifier_train_dev'
+
+    @staticmethod
+    def sort_key(ex):
+        return len(ex.text)
+
+    def __init__(self, text_field, label_field, path=None, examples=None, **kwargs):
+        """Create a YELP dataset instance given a path and fields.
+
+        Arguments:
+            text_field: The field that will be used for text data.
+            label_field: The field that will be used for label data.
+            path: Path to the data file.
+            examples: The examples contain all the data.
+            Remaining keyword arguments: Passed to the constructor of
+                data.Dataset.
+        """
+        # def clean_str(string):
+        #     """
+        #     Tokenization/string cleaning for all datasets except for SST.
+        #     Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
+        #     """
+        #     string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+        #     string = re.sub(r"\'s", " \'s", string)
+        #     string = re.sub(r"\'ve", " \'ve", string)
+        #     string = re.sub(r"n\'t", " n\'t", string)
+        #     string = re.sub(r"\'re", " \'re", string)
+        #     string = re.sub(r"\'d", " \'d", string)
+        #     string = re.sub(r"\'ll", " \'ll", string)
+        #     string = re.sub(r",", " , ", string)
+        #     string = re.sub(r"!", " ! ", string)
+        #     string = re.sub(r"\(", " \( ", string)
+        #     string = re.sub(r"\)", " \) ", string)
+        #     string = re.sub(r"\?", " \? ", string)
+        #     string = re.sub(r"\s{2,}", " ", string)
+        #     return string.strip()
+        #
+        # text_field.preprocessing = data.Pipeline(clean_str)
+        fields = [('text', text_field), ('label', label_field)]
+
+        if examples is None:
+            path = self.dirname if path is None else path
+            examples = []
+            with open(os.path.join(path, 'yelp.neg'), errors='ignore') as f:
+                examples += [
+                    data.Example.fromlist([line, 'negative'], fields) for line in f]
+            with open(os.path.join(path, 'yelp.pos'), errors='ignore') as f:
+                examples += [
+                    data.Example.fromlist([line, 'positive'], fields) for line in f]
+        super(YELP, self).__init__(examples, fields, **kwargs)
+
+    @classmethod
+    def splits(cls, text_field, label_field, dev_ratio=.1, shuffle=True, root='.', **kwargs):
+        """Create dataset objects for splits of the MR dataset.
+
+        Arguments:
+            text_field: The field that will be used for the sentence.
+            label_field: The field that will be used for label data.
+            dev_ratio: The ratio that will be used to get split validation dataset.
+            shuffle: Whether to shuffle the data before split.
+            root: The root directory that the dataset's zip archive will be
+                expanded into; therefore the directory in whose trees
+                subdirectory the data files will be stored.
+            train: The filename of the train data. Default: 'train.txt'.
+            Remaining keyword arguments: Passed to the splits method of
+                Dataset.
+        """
+        # path = cls.download_or_unzip(root)
+        path = root
+        examples = cls(text_field, label_field, path=path, **kwargs).examples
+        if shuffle: random.shuffle(examples)
+        dev_index = -1 * int(dev_ratio*len(examples))
+
+        return (cls(text_field, label_field, examples=examples[:dev_index]),
+                cls(text_field, label_field, examples=examples[dev_index:]))
