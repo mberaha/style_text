0f5f5cc2a0fa09c0180913b0f537a5db4e6b8071
mariob6 <berahamario@gmail.com>
2018-06-29 11:38:44 +0200

    logging everything to file

--- scripts/train_yelp.sh
+++ scripts/train_yelp.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-python3 -m scripts.train \
+python3 -m scripts.train_model \
   --train_file_style1 data/yelp/train/positive_sentence.txt \
   --train_file_style2 data/yelp/train/negative_sentence.txt \
   --evaluation_file_style1 data/yelp/dev/positive.txt \
   --evaluation_file_style2 data/yelp/dev/negative.txt \
-  --savefile edata/models/yelp/model
+  --vocabulary data/yelp/vocabulary.pickle \
+  --savefile data/models/yelp/model \
+  --logdir data/models/yelp/log/
