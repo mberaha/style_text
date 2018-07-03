38ef1726a0fd8d5fb2998c0136c9c4c2ddbdc72f
WolfLo <lorenzo.lupo93@gmail.com>
2018-07-03 14:08:32 +0200

    fixed losses

--- scripts/train_yelp_local.sh
+++ scripts/train_yelp_local.sh
@@ -9,8 +9,8 @@ fi
 python3 -m scripts.train_model \
   --train_file_style1 data/yelp/test/positive_sentence.txt \
   --train_file_style2 data/yelp/test/negative_sentence.txt \
-  --evaluation_file_style1 data/test/positive_sentence.txt \
-  --evaluation_file_style2 data/test/negative_sentence.txt \
+  --evaluation_file_style1 data/yelp/test/positive_sentence.txt \
+  --evaluation_file_style2 data/yelp/test/negative_sentence.txt \
   --vocabulary data/yelp/vocabulary.pickle \
   --savefile data/models/yelp/model \
   --logdir data/models/yelp/log/ \
