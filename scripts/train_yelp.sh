66cce48fe0e7a0922b9279d91914a14b8596ef56
mariob6 <berahamario@gmail.com>
2018-10-14 10:00:50 +0200

    final cleanup

--- scripts/train_yelp.sh
+++ scripts/train_yelp.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-MACHINE=${1}
-
-if [ -z "${MACHINE}" ]; then
-  MACHINE="local"
-fi
-
 python3 -m scripts.train_model \
   --train_file_style1 data/yelp/debug/positive_sentence.txt \
   --train_file_style2 data/yelp/debug/negative_sentence.txt \
@@ -13,5 +6,4 @@ python3 -m scripts.train_model \
   --evaluation_file_style2 data/yelp/debug/negative_sentence.txt \
   --vocabulary data/yelp/vocabulary.pickle \
   --savefile data/models/yelp/model \
-  --logdir data/models/yelp/log/ \
-  --machine ${MACHINE}
+  --logdir data/models/yelp/log/
